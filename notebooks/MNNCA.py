masking outer radius and fixing some precision problems..sort of

notebooks/MNNCA.py
@@ -8,9 +8,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-HNM_STEPS = 3000  # Max number of steps to mine for a hard-negative sample before while-loop terminates
-
-
 def totalistic(x, dim2=False):
     if dim2:
         y_idx = 0
@@ -56,9 +53,17 @@ class Rule(nn.Module):
         z = torch.randn(1, dim_z).cuda()
         coords = self.cppn._coordinates(5, Rk, Rk, z)
 
+        coords[0] = 10 + coords[2] * 2
+        coords[1] = 10 + coords[2] / 2
+        coords[2] = 10 + 5 * coords[2]
+
         # coords[0] = coords[2] * 2
-        # coords[1] = coords[2] / 2
-        coords[2] = 5 * coords[2] * coords[2]
+        # coords[1] = torch.cos(coords[2] / 2)
+        # coords[2] = torch.sin(5 * coords[2] * coords[2])
+
+        xm, ym = torch.meshgrid(torch.linspace(-1, 1, Rk), torch.linspace(-1, 1, Rk))
+        rm = torch.sqrt(xm ** 2 + ym ** 2).cuda()
+        null = torch.zeros_like(rm).cuda()
 
         kernels = []
         for i in range(FILTERS):
@@ -66,7 +71,9 @@ class Rule(nn.Module):
             # k = (self.cppn.forward(coords, Rk, Rk).reshape(1, Rk, Rk, dim_c).permute(0, 3, 1, 2) > 0.9).type(torch.cuda.FloatTensor)
             k = (self.cppn.forward(coords, Rk, Rk).reshape(1, Rk, Rk, dim_c).permute(0, 3, 1, 2))
             k = k.repeat((1, CHANNELS, 1, 1))
+            k = torch.where(rm < 0.9, k, null)
             k = k - k.mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
+            k = torch.where(rm < 0.9, k, null)
             # k = k * (k.abs() > 0.5).type(torch.cuda.FloatTensor)
             # k = k / k.norm()
             kernels.append(nn.Parameter(k))
@@ -78,33 +85,39 @@ class Rule(nn.Module):
         self.transitions = nn.ModuleList()
         for j in range(FILTERS):
             layers = []
-            # layers.append(SwapC_with_Last())
             for i in range(len(net_size[:-1])):
-                # layers.append(nn.LocalResponseNorm(3))
-                # layers.append(SwapC_with_Last())
-                # layers.append(nn.Linear(net_size[i], net_size[i + 1], bias=False) )
-                layers.append(nn.Conv2d(net_size[i], net_size[i + 1], 1, padding_mode='circular', bias=False))
-                # nn.init.orthogonal_(layers[-1].weight)
-                # layers.append(SwapC_with_Last())
-                nn.init.normal_(layers[-1].weight)
-                # nn.init.sparse_(layers[-1].weight, sparsity=0.9, std=1)
                 if (i + 1) < len(net_size):
-                    # layers.append(nn.LeakyReLU())
-                    # layers.append(nn.ReLU())
-                    layers.append(nn.Tanh())
-                    # layers.append(nn.Hardsigmoid())
-
-
-
-            # layers.append(nn.ReLU())
-            # layers.append(SwapC_with_Last())
-            # layers.append(nn.Sigmoid())
+                    activation = True
+                else:
+                    activation = False
+                layers.append(MNCA_block(net_size[i], net_size[i + 1], activation=activation))
 
             seq = nn.Sequential(*layers)
             self.transitions.append(seq)
 
 
         ###########################################
+
+class MNCA_block(nn.Module):
+
+    def __init__(self, in_channels, out_channels, activation=True):
+        super().__init__()
+        self.PrecisionValue = torch.floor(torch.tensor([2 ** 31 / (128)])).cuda()
+        self.activation = activation
+        self.conv = nn.Conv2d(in_channels, out_channels, 1, padding_mode='circular', bias=False)
+        nn.init.orthogonal_(self.conv.weight)
+        # nn.init.normal_(layers[-1].weight)
+        # nn.init.sparse_(layers[-1].weight, sparsity=0.9, std=1)
+        self.afunc = nn.Tanh()
+
+    def forward(self, x):
+        x = torch.floor(x * self.PrecisionValue)
+        x = self.conv(x)
+        x = torch.floor(x) / self.PrecisionValue
+        if self.activation:
+            x = self.afunc(x)
+
+        return x
 
 class CA(nn.Module):
     def __init__(self, CHANNELS=8, FILTERS=1, NET_SIZE=[32, 32, 32], RADIUS=2):
@@ -113,7 +126,8 @@ class CA(nn.Module):
         self.filters = FILTERS
         self.radius = RADIUS
 
-
+        Rk = 2 * RADIUS + 1
+        self.PrecisionValue = torch.floor(torch.tensor([2 ** 31 / (Rk * Rk * 128)])).cuda()
         self.rule = Rule(CHANNELS, FILTERS, NET_SIZE, RADIUS)
         # self.optim = torch.optim.Adam(self.parameters(), lr=1e-3)
 
@@ -126,12 +140,6 @@ class CA(nn.Module):
         return seed
 
     def forward(self, x, update_rate=1):
-        '''
-        The first filter applies a depthwise convolution to the CA grid. Each channel in the filter is applied to its corresponding channel in the CA grid.
-        The second and third filters are 1x1 convolutions which act to mix the channels.
-        If I understand this correctly, this is essentially applying a depthwise seperable convolution operation on the input (but I am a bit uncertain).
-        PRETTY SURE THIS IS WRONG, RE-WRITE THIS!!
-        '''
         # circular/gaussian kernel mask
         kernels = self.rule.kernels
         if self.rule.totalistic:
@@ -140,9 +148,12 @@ class CA(nn.Module):
         bias = [b for b in self.rule.bias]
         R = self.radius
 
-        z = F.pad(x, (R, R, R, R), 'circular')
+        z = torch.floor(x * self.PrecisionValue)
+
+        z = F.pad(z, (R, R, R, R), 'circular')
 
         perceptions = [F.conv2d(z, weight=kernels[i], bias=bias[i], padding=0) for i in range(len(kernels))]
+        perceptions = [torch.floor(p) / self.PrecisionValue for i, p in enumerate(perceptions)]
         out = []
         for i, p in enumerate(perceptions):
             out.append(self.rule.transitions[i](p))
@@ -150,7 +161,7 @@ class CA(nn.Module):
         # min_idx = torch.argmin(z, dim=0, keepdim=True)
         # z = torch.gather(z, 0, min_idx)[0]
         idx = torch.argsort(z, dim=0)
-        z = torch.gather(z, 0, idx)[-2]
+        z = torch.gather(z, 0, idx)[-3]
         # z = torch.stack(out).mean(0) * update_rate
 
         # for i in range(len(kernels)):